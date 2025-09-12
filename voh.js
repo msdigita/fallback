8f7802ba7019cdd5cb4026bb9b8640c11f81aa9a
msdigita <141251581+msdigita@users.noreply.github.com>
2025-09-12 14:56:03 +0700

    Update voh.js
    add prefix voh_

--- voh.js
+++ voh.js
@@ -4,12 +4,12 @@
         function findBestTemplate(width, height) {
             const targetRatio = height / width;
             const templates = [
-                { width: 300, height: 600, url: 'https://msdigita.github.io/fallback/300x600.html' },
-                { width: 300, height: 250, url: 'https://msdigita.github.io/fallback/300x250.html' },
-                { width: 728, height: 90,  url: 'https://msdigita.github.io/fallback/728x90.html' },
-                { width: 390, height: 390, url: 'https://msdigita.github.io/fallback/390x390.html' }
+                { width: 300, height: 600, url: 'https://msdigita.github.io/fallback/voh_300x600.html' },
+                { width: 300, height: 250, url: 'https://msdigita.github.io/fallback/voh_300x250.html' },
+                { width: 728, height: 90,  url: 'https://msdigita.github.io/fallback/voh_728x90.html' },
+                { width: 390, height: 390, url: 'https://msdigita.github.io/fallback/voh_390x390.html' }
             ];
-            const defaultTemplate = { url: 'https://msdigita.github.io/fallback/responsive.html' };
+            const defaultTemplate = { url: 'https://msdigita.github.io/fallback/voh_responsive.html' };
             
             let bestMatch = null;
             let smallestDiff = Infinity;
